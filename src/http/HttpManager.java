4ccf4c195b20b2769afc019bc55c15f866a119ae
shahaal <Alban.SHAHAJ@ext.efsa.europa.eu>
2018-11-05 17:06:41 +0100

    Compiled as Maven project with SWT/JFace internal IDE libraries

--- src/http/HttpManager.java
+++ src/http/HttpManager.java
@@ -13,11 +13,11 @@ import java.net.URL;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import com.btr.proxy.search.ProxySearch;
-import com.btr.proxy.search.ProxySearch.Strategy;
+import com.github.markusbernhardt.proxy.ProxySearch;
+import  com.github.markusbernhardt.proxy.ProxySearch.Strategy;
 
 import config.ProxyConfig;
 import proxy.ProxyConfigException;
